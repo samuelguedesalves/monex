47d573391138cb60f97ed931a4cfae3322c373af
Samuel Guedes <guedes.works7@gmail.com>
2022-07-18 17:21:54 -0400

    chore: add user factory

--- backend/test/support/factory.ex
+++ backend/test/support/factory.ex
@@ -11,4 +11,14 @@ defmodule Monex.Factory do
       "password" => "123456"
     }
   end
+
+  def user_factory do
+    %User{
+      name: "Samuel",
+      cpf: "27024081171",
+      initial_amount: 200_000,
+      password: "123456",
+      password_hash: Pbkdf2.hash_pwd_salt("123456")
+    }
+  end
 end
